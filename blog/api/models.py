832e2fc569665334d0a191c9f50041c50b2ddfc9
Aniket <aniketjadhav310@gmail.com>
2024-09-20 23:06:18 +0530

    only few line add

--- blog/api/models.py
+++ blog/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class BlogModel(models.Model):
+    title=models.CharField(max_length=30)
+    content=models.TextField()
+    image=models.ImageField()
