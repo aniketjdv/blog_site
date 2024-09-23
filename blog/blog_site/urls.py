39e603d9ea07d5b066b091bd3fb5cba9bad19c9d
Aniket <aniketjadhav310@gmail.com>
2024-09-23 09:27:26 +0530

    views created

--- blog/blog_site/urls.py
+++ blog/blog_site/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name="homepage")
+    path('',views.home,name="homepage"),
+    path('create/',views.blog_create,name="blog_create"),
+    path('delete/',views.blog_delete,name="blog_delete"),
 ]
--- blog/blog_site/views.py
+++ blog/blog_site/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from api.models import BlogModel
 from .forms import BlogForm
 
@@ -7,5 +7,36 @@ def home(request):
     blogs=BlogModel.objects.all().order_by('-created_at')
     return render(request,'index.html',{'blogs':blogs})
 
-def create(request):
-    pass
+def blog_create(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST,request.FILES)
+        if form.is_valid:
+            blog = form.save(commit=False)
+            blog.user=request.user
+            blog.save()
+            return redirect('homepage')
+    else:
+        form = BlogForm()
+    return render(request,'blog_form.html',{'form':form})
+
+def blog_edit(request,blog_id):
+    blog = get_object_or_404(BlogModel,pk=blog_id,user = request.user)
+    if request.method == "POST":
+        form = BlogForm(request.POST,request.FILES,instance=blog)
+        if form.is_valid:
+            blog = form.save(commit=False)
+            blog.user=request.user
+            blog.save()
+            return redirect('homepage')    
+    else:
+        form = BlogForm(instance=blog)
+    return render(request,'blog_form.html',{'form':form})
+
+
+def blog_delete(request,blog_id):
+    blog=get_object_or_404(BlogModel,pk=blog_id,user=request.user)
+    if request.method == "POST":
+        blog.delete()
+        return redirect('homepage')
+    
+    return render(request,'blog_confirm_delete.html',{'blog':blog})   
